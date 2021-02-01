35023ced3799cc613b2e83459fa265fa6a3cd796
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 15:51:09 -0800

    Job Search List

--- react-job-board/src/components/JobList.js
+++ react-job-board/src/components/JobList.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 
 class JobList extends Component {
     render(){
         return(
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                {this.props.jobs.map(job => <ul><li>{job.title}</li></ul>)}
+                {this.props.jobs.map(job => 
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>{job.title}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                )}
             </div>
         )
     }
