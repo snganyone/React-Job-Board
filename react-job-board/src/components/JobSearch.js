8d2521a5f23fc001b813f1de7465a584505ee13a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-31 18:51:48 -0800

    Job Search

--- react-job-board/src/components/JobSearch.js
+++ react-job-board/src/components/JobSearch.js
@@ -5,12 +5,18 @@ class JobSearch extends Component{
         query: ""
     }
 
+    handleQueryChange = event => {
+        this.setState({
+            query: event.target.value
+        });
+    }
+
     render(){
         return(
             <div>
                 <form>
                     <label>Enter a Search Term:</label><br></br>
-                    <input type="text" /><br></br>
+                    <input type="text" value={this.state.query}/><br></br>
                     <input type="submit" />
                 </form>
             </div>
