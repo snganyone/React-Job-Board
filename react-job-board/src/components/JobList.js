a8226844f15db27465bfbe9ec9f6070389ca4588
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 13:22:40 -0800

    React Bootstrap Search

--- react-job-board/src/components/JobList.js
+++ react-job-board/src/components/JobList.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class JobList extends Component {
     render(){
         return(
-            <div>{this.props.jobs.map(job => job.title)}</div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                {this.props.jobs.map(job => job.title)}
+            </div>
         )
     }
 }
