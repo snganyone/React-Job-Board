22477fb6047a5989c3465b9094927c86c3388214
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 15:16:47 -0800

    Landing Page Navbar

--- react-job-board/src/containers/HomeContainer.js
+++ react-job-board/src/containers/HomeContainer.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class HomeContainer extends Component{
     render(){
         return(
             <div>
                 <Navbar bg="light">
-                    <Navbar.Brand href="#home">Relier</Navbar.Brand>
+                    <Navbar.Brand href="/">Relier</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/jobs">Jobs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
-            Home Container
             </div>
         )
     }
