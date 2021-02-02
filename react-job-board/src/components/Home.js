57c5558ce877c26bd88d3f9cd28efcba250e013e
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 17:50:01 -0800

    Landing Page

--- react-job-board/src/components/Home.js
+++ react-job-board/src/components/Home.js
@@ -6,7 +6,14 @@ class Home extends Component{
         return(
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
                 Home Page
-                <Card>
+                <Card className="text-center">
+                    <Card.Header>Welcome to Relier</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Welcome To Relier</Card.Title>
+                        <Card.Text>
+                            An online Job dedicated to help you find the perfect fit.
+                        </Card.Text>
+                    </Card.Body>
                 </Card>
             </div>
         )
