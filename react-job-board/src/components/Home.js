6eca4193e03af8135909bb754ff4c67ef299947d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 17:53:59 -0800

    Landing Page

--- react-job-board/src/components/Home.js
+++ react-job-board/src/components/Home.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Home extends Component{
     render(){
         return(
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
                 <Card className="text-center">
-                    <Card.Header>Welcome to Relier</Card.Header>
+                    <Card.Header>Relier</Card.Header>
                     <Card.Body>
                         <Card.Title>Welcome To Relier</Card.Title>
                         <Card.Text>
                             An online Job dedicated to help you find the perfect fit.
                         </Card.Text>
+                        <Button variant="primary" href="/jobs">Find Jobs</Button>
                     </Card.Body>
                     <Card.Footer className="text-muted">Relier helps people find jobs</Card.Footer>
                 </Card>
