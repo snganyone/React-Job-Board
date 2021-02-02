661c51a78c6f9ec3972fd193f403a963062e9d91
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-01 19:17:05 -0800

    Job Input Form

--- react-job-board/src/components/JobForm.js
+++ react-job-board/src/components/JobForm.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
 //import Button from 'react-bootstrap/Button';
+//import { Col } from 'react-bootstrap';
 
 class JobForm extends Component{
+
+    state = {
+        text: ''
+    }
+
     render(){
         return(
-            <div>Job Input Form</div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <Form>
+                    <Form.Row> 
+                    </Form.Row>
+                </Form>
+            </div>
         )
     }
 }
