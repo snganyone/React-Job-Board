ec2347ae5ca9e3b9f3645ac933869546c886f7ab
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-03 20:27:14 -0800

    Delete Job

--- react-job-board-backend/app/controllers/jobs_controller.rb
+++ react-job-board-backend/app/controllers/jobs_controller.rb
@@ -9,10 +9,21 @@ class JobsController < ApplicationController
         end
     end
     
-        def create
-            @job = Job.create(job_params)
-            render json: @job, except: [:created_at, :updated_at]
+    def create
+        @job = Job.create(job_params)
+        render json: @job, except: [:created_at, :updated_at]
+    end
+
+    def destroy 
+        @job = Job.find(params[:id])
+        if @job
+            @job.destroy
+            render json: {message: "Job Deleted", id: @job.id}
+        else
+            render json: {error: "Unable to delete Job Post"}
         end
+    end
+
 
     def job_params
 		params.require(:job).permit(:title, :employer, :location, :description, :release_date, :job_type)
