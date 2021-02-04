b7a61be26debe53d9157f7384b3eb747c8016476
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-03 20:40:24 -0800

    Delete Job

--- react-job-board/src/actions/actionCreator.js
+++ react-job-board/src/actions/actionCreator.js
@@ -22,3 +22,17 @@ export const addJob = (job) => {
             })
     }
 }
+
+export const deleteJob = (job) => {
+    return (dispatch) => {
+        fetch("http://localhost:4000/jobs", {
+            method: "POST",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({job})
+        })
+        .then(res => {return res.json()})
+        .then((data) => {
+            dispatch({type: 'DELETE_JOB', id: data.id})
+            })
+    }
+}
