17683392b0462ea3609842da4cac2b4eb571ec9c
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-06 20:34:05 -0800

    Nav Menu

--- react-job-board/src/App.js
+++ react-job-board/src/App.js
@@ -3,25 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import JobContainer from '../src/containers/JobContainer';
 import Home from '../src/components/Home';
 import JobForm from '../src/components/JobForm';
-
-
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import NavMenu from '../src/components/NavMenu'
 
 class App extends Component{
   render(){
     return(
       <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Relier</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="/jobs">Jobs</Nav.Link>
-                <Nav.Link href="/add-job">Post a Job</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        <NavMenu />
         <Router>
         <Route exact path="/" component={Home}/>
         <Route exact path="/jobs" component={JobContainer}/>
---
+++ react-job-board/src/components/NavMenu.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class NavMenu extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">Relier</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/jobs">Jobs</Nav.Link>
+                        <Nav.Link href="/add-job">Post a Job</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+          </div>
+        )
+    }
+}
+
+export default NavMenu;
