d19f41a64b5398d3b00444aefc1701497f1e7f83
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-06 20:45:03 -0800

    Footer

--- react-job-board/src/App.js
+++ react-job-board/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import JobContainer from '../src/containers/JobContainer';
 import Home from '../src/components/Home';
 import JobForm from '../src/components/JobForm';
-import NavMenu from '../src/components/NavMenu'
+import NavMenu from '../src/components/NavMenu';
+import Footer from '../src/components/Footer';
 
 class App extends Component{
   render(){
     return(
       <div>
         <NavMenu />
+        <Footer />
         <Router>
         <Route exact path="/" component={Home}/>
         <Route exact path="/jobs" component={JobContainer}/>
