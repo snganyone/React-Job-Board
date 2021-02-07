8339fef2d670d767f00012920176217b5144abf3
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-06 21:08:10 -0800

    Footer

--- react-job-board/src/components/Footer.js
+++ react-job-board/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component{
     render(){
         return(
-            <div class="footer" style={{position: 'absolute', bottom: '0px'}}>
+            <div class="footer" style={{position: 'absolute', bottom: '0px', width: '100px', height: '60px'}}>
                 Footer
             </div>
         )
