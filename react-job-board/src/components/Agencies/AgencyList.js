183ac48a6c4e474f039b03f3f4cbc81c78dc6e4c
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-08 20:06:07 -0800

    Agency Component

--- react-job-board/src/components/Agencies/AgencyList.js
+++ react-job-board/src/components/Agencies/AgencyList.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 class AgencyList extends Component {
     render(){
-        return(<div>{this.props.agencies.map(a => a.name)}</div>)
+        return(
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+            {this.props.agencies.map(a => a.name)}
+        </div>)
     }
 }
 
