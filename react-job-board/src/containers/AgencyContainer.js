584d6ade13bde92a6f7f6ded49959d902713e7ce
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-08 17:25:20 -0800

    Agency Container

--- react-job-board/src/containers/AgencyContainer.js
+++ react-job-board/src/containers/AgencyContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAgencies } from '../actions/agencyCreator';
 
 class AgencyContainer extends Component{
     render(){
@@ -6,4 +8,11 @@ class AgencyContainer extends Component{
     }
 }
 
-export default AgencyContainer;
+
+const mapDispatchToProps = dispatch => {
+    return{
+        fetchAgencies: () => dispatch(fetchAgencies())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(AgencyContainer);
