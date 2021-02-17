a8bf2c7242cf661700061e0e2b753886cd8a0572
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-17 13:03:14 -0800

    Job Show

---
+++ react-job-board/src/components/Jobs/JobShow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class JobShow extends Component{
+    render(){
+        return(
+            <div>Show Page</div>
+        )
+    }
+}
+
+export default JobShow;
