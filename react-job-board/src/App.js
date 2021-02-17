f18c876d41fa29b96de0956cec40baa692d62c8b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-17 13:09:38 -0800

    Job Show

--- react-job-board/src/App.js
+++ react-job-board/src/App.js
@@ -6,6 +6,7 @@ import JobForm from '../src/components/Jobs/JobForm';
 import NavMenu from '../src/components/NavMenu';
 import Footer from '../src/components/Footer';
 import AgencyContainer from '../src/containers/AgencyContainer';
+import JobShow from '../src/components/Jobs/JobShow';
 
 class App extends Component{
   render(){
@@ -16,6 +17,7 @@ class App extends Component{
         <Router>
         <Route exact path="/" component={Home}/>
         <Route exact path="/jobs" component={JobContainer}/>
+        <Route exact path="/job" component={JobShow}/>
         <Route exact path="/add-job" component={JobForm}/>
         <Route exact path="/agencies" component={AgencyContainer}/>
         </Router>
