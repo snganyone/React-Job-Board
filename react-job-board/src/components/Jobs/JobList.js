fd475ef9a586f79312e2173f00a72147854f0ac0
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-17 13:50:08 -0800

    Job Show

--- react-job-board/src/components/Jobs/JobList.js
+++ react-job-board/src/components/Jobs/JobList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 import { deleteJob } from '../../actions/jobCreator';
 import { showJob } from '../../actions/jobCreator';
@@ -12,7 +13,9 @@ class JobList extends Component {
         this.props.deleteJob(id);
     }
 
-    handleShow = id => {}
+    handleShow = id => {
+        this.props.showJob(id)
+    }
 
     render(){
         console.log(this.props)
@@ -25,6 +28,7 @@ class JobList extends Component {
                                 <Card.Subtitle>{job.employer}</Card.Subtitle>
                                 <Card.Text>{job.description}</Card.Text>
                             </Card.Body>
+                            <Link to='/job' id={job.id}>Job Link</Link>
                             <Button variant="danger" onClick={() => this.props.deleteJob(job.id)} id={job.id}>Delete Job</Button>
                         </Card>
                 )}
