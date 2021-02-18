e58ff6018e696e9e5d2c8ebc9af67bd0e8af8c79
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-02-17 18:23:00 -0800

    Job Show Page

--- react-job-board/src/components/Jobs/JobPage.js
+++ react-job-board/src/components/Jobs/JobPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 const JobPage = (props) => {
     return(
     <div>
-        {props.jobs.map(j => j.title)}
+        <h3>Job Description</h3>
+            {props.jobs.map(j => 
+            <Jumbotron>
+                {j.title}
+            </Jumbotron>
+            )}
     </div>
     )
 }
